7546ead8eb4303d3e83a71aa4cd9b506ecc7035e
keyan <keyan.kousha+huumn@gmail.com>
2023-09-05 23:37:12 -0400

    fix no webs exist

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default async function Page () {
     <div className='flex flex-col py-10'>
       <Link className='text-blue-600 text-center visited:text-purple-600' href='/web'>create new</Link>
       <span className='mt-10 mb-5 text-center font-semibold'>or join existing web</span>
-      {webs.map(({ name }) =>
+      {webs?.map(({ name }) =>
         <Link className='text-blue-600 text-center visited:text-purple-600' key={name} href={`/${name}`}>{name}</Link>)}
     </div>
   )
